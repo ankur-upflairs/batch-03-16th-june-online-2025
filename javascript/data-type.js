9faa1d3c78a18cf31d02eb3cca962c24afbc163d
ankur-upflairs <ankur.upflairs@gmail.com>
2025-06-30 12:00:50 +0530

    data

---
+++ javascript/data-type.js
@@ -0,0 +1,47 @@
+//variable => it is a  name of memory location where value is stored
+//declare - let , var , const 
+let a; //semicolons are optional 
+a =5
+//variable => contains alphabets , number , $ , _
+//can not start with no.
+
+// 1.number 
+let b = 23.5;
+// console.log(b,typeof b)
+// 2.string 
+let c = 'hello'
+let d = "everyone"
+// console.log(c, typeof c)
+// 3. boolean
+let e = true 
+let f = false 
+// console.log(e, f , typeof e)
+// 4. undefined 
+let g;
+// console.log(typeof g)
+// 5. null 
+let h = null 
+// console.log(typeof null)
+// 6. bigInt 
+let i = 2324344354n
+// console.log(i , typeof i)
+// 7. symbol 
+let s1 = Symbol('a')
+let s2 = Symbol('a')
+console.log(s1 == s2)
+
+// object =>
+let person = {
+    name: 'Gagan',
+    age : 45
+}
+console.log(person.age , person.name)
+
+
+
+
+
+
+
+
+
