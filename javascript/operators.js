17524b0b3a59a9a76bed9b92a0ce227408bf9de7
ankur-upflairs <ankur.upflairs@gmail.com>
2025-06-30 12:26:45 +0530

    operators

---
+++ javascript/operators.js
@@ -0,0 +1,41 @@
+//arithmatic operators => + , - , * , / , % (modulus), ** (exponential)
+// console.log(14 % 6)
+// console.log(2**4)
+// assignment operators =>
+let a = 5
+//+= ,-= , *= , /= , %=
+a*=3 //a = a*3 
+// console.log(a) 
+a%=3 //a = a % 3 
+// console.log(a) 
+//comparison / relational operators
+//compare two values and return boolean 
+
+// console.log(4 > 6)
+// console.log(4 < 6)
+// console.log( 4 >= 6)
+// console.log(4 <= 6 )
+// console.log(4==6)
+// console.log(4 != 6)
+
+//=== (strict checking ) checks value as well as datatype
+// console.log(5=='5' , 5==='5')
+// logical operators => compare two boolean values and return boolean
+//and && => if both sides are true than it return true otherwise false
+// console.log(false && false)
+// console.log(false && true)
+// console.log(true && true)
+// console.log(true && false)
+
+// or || => if any side is true than return true  otherwise false 
+// console.log(false || false) 
+// console.log(false || true) 
+// console.log(true || true)
+// console.log(true || false)
+// not ! => reverse the result 
+//falsy => 0 , '' (blank string) , false , undefined , null 
+
+console.log(!5)
+
+
+
