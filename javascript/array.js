9787bef8d291ff244db763b17f6fd5bc2da1999a
ankur-upflairs <ankur.upflairs@gmail.com>
2025-07-03 13:15:09 +0530

    assignment

--- javascript/array.js
+++ javascript/array.js
@@ -16,17 +16,64 @@ let b = [2,5,7,8,5,4]
 // console.log(b.slice(1,4))
 // console.log(b.splice(1,0,23,45))
 // console.log(b)
-for(let i in b ){
-    console.log(i,b[i])
-}
-for(let x of b ){
-    console.log(x)
-}
+// for(let i in b ){
+//     console.log(i,b[i])
+// }
+// for(let x of b ){
+//     console.log(x)
+// }
 //insert element at specified location in an array 
 //delete element at specified location in an array 
 //take 5 values from user and create an array from them 
 //and print them in reverse order 
 //flatten an 2d array (without method)
+// console.log('abc'.padStart(6,'.'))
+// let arr = [2,4,6,1,23,67]
+// arr.forEach((value,index,array)=>{
+//         console.log(value,index,array)
+// })
+//find the sum of array using forEach 
+// console.log(arr.map((v)=>v*3))
+
+//create a new array from given array as :- 
+// [3,4,5,7] => [{a:3},{b:4},{c:5},{d:7}]
+
+
+// let newArray = arr.map((v,i)=>{
+//     let key = String.fromCharCode(65+i)
+//     // console.log(key)
+//     return {[key]:v} 
+// })
+// console.log(newArray)
+let arr = [2,4,6,1,23,67]
+
+// console.log(arr.every(v=>v>1))
+// console.log(arr.some(v=>v>29))
+// console.log(arr.filter(v=>v>5))
+// console.log(arr.find(v => v> 20))
+// console.log(arr.findIndex(v => v> 20))
+// console.log(arr.sort((a,b)=>b-a))
+// console.log(arr.reduce((preValue,value,index,array)=>{
+//     return preValue + value
+// } , 0 ))
+//p-0 , 2 => p-2 , 4 => p-6 ,6
+
+// find the multiplication , average using reduce 
+//take 5 ages array from user and filter these with 18+
+// create an array and ask user for index to delete element 
+// create an array and ask user for element to delete element 
+// make a array (string + number ) and sort them ,
+//  infirst write nos and in last write strings
+
+
+
+
+
+
+
+
+
+
 
 
 
